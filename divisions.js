cda05c183e4215a4c8968dcf4ffc831f4e676884
AgentK0081 <harryjoji08@gmail.com>
2025-12-28 18:31:59 +0530

    Create divisions.js

---
+++ divisions.js
@@ -0,0 +1,33 @@
+const divisions = [
+  {
+    name: "Fugitive Task Force",
+    subtitle: "Public Roleplay Division",
+    image: "https://i.imgur.com/9QO6p5C.png",
+    discord: "https://discord.gg/yourinvite1"
+  },
+  {
+    name: "Sheriff",
+    subtitle: "Liberty County Sheriff's Office",
+    image: "https://i.imgur.com/xG5FQ8n.png",
+    discord: "https://discord.gg/yourinvite2"
+  },
+  {
+    name: "Firefighter",
+    subtitle: "River City Fire & Rescue",
+    image: "https://i.imgur.com/7oXzZ5Y.png",
+    discord: "https://discord.gg/yourinvite3"
+  },
+  {
+    name: "Police",
+    subtitle: "River City Police Department",
+    image: "https://i.imgur.com/1YxYwqf.png",
+    discord: "https://discord.gg/yourinvite4"
+  },
+  {
+    name: "DOT",
+    subtitle: "Department of Transportation",
+    image: "https://i.imgur.com/LsZKz7H.png",
+    discord: "https://discord.gg/yourinvite5"
+  }
+];
+
